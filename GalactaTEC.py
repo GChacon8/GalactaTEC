a84263b90bc8fa775b5da2dfa6f26cdb3de3d764
Dcastroe10 <dcastroeli@estudiantec.cr>
2024-04-20 12:29:31 -0600

    Moving Background added

--- GalactaTEC.py
+++ GalactaTEC.py
@@ -1,5 +1,7 @@
 import pygame
 import tkinter as tk
+import itertools
+import os
 import random
 import sys
 
@@ -67,15 +69,46 @@ class Ship:
     screen.blit(self.image, self.rect)
 
 
+class AnimatedBackground(pygame.sprite.Sprite):
+    def __init__(self, position, images, delay):
+        super(AnimatedBackground, self).__init__()
+
+        self.images = itertools.cycle(images)
+        self.image = next(self.images)
+        self.rect = pygame.Rect(position,  self.image.get_rect().size)
+
+        self.animation_time = delay
+        self.current_time = 0
+
+    def update(self, dt):
+        self.current_time += dt
+        if self.current_time >= self.animation_time:
+            self.current_time = 0
+            self.image = next(self.images)
+
+
+def load_images(path):
+    images =  [pygame.image.load(path + os.sep + file_name).convert() for file_name in sorted(os.listdir(path))]
+    return images
+
+
 class game:
   def __init__(self):
     pygame.init()
     self.width = 800
     self.height = 600
     self.screen = pygame.display.set_mode((self.width, self.height))
+
+    self.images = [pygame.image.load('background_frames' + os.sep + file_name).convert() for file_name in sorted(os.listdir('background_frames'))]
+    self.background = AnimatedBackground(position=(0, 0), images=self.images, delay = 0.03)
+    self.all_sprites = pygame.sprite.Group(self.background)
+
+
+
     pygame.display.set_caption("GalactaTEC")
-    self.background = pygame.Surface(self.screen.get_size())
-    self.background.fill((255, 255, 255))  # Blanco
+    #self.background = pygame.Surface(self.screen.get_size())
+    #self.background.fill((255, 255, 255))  # Blanco
+
     self.inst_ship = Ship()
     self.inst_enemies = EnemyFactory.create_enemies(6,6)
     self.setup_counter = 0
@@ -84,8 +117,12 @@ class game:
   
 
   def run(self):
-    
     while self.running:
+
+      dt=pygame.time.Clock().tick(60)
+      
+
+
     # Manejo de eventos
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -99,10 +136,16 @@ class game:
 
       
       
-  
+      self.all_sprites.update(dt)    
 
     # Dibujar todo
-      self.screen.blit(self.background, (0, 0))  # Dibujar fondo
+      self.screen.blit(self.background.image, self.background.rect)  # Dibujar fondo
+
+      self.all_sprites.draw(self.screen)
+       
+
+
+
       self.inst_ship.draw(self.screen)  # Dibujar la nave en su nueva posición
 
       #"""
@@ -124,9 +167,7 @@ class game:
               
     # Actualizar la pantalla
       pygame.display.flip()
-
-    # Pequeña pausa para controlar la velocidad de fotogramas
-      pygame.time.Clock().tick(60)
+      
 
 if __name__ == "__main__":
   inst_galacta = galacta()
