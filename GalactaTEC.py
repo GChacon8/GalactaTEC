8273f7fa250c660f3f19fa81e5bb15025974a370
valeriam07 <47421582+valeriam07@users.noreply.github.com>
2024-04-21 13:19:17 -0600

    Estética del menú actualizada

--- GalactaTEC.py
+++ GalactaTEC.py
@@ -2,26 +2,73 @@ import pygame
 import tkinter as tk
 import random
 import sys
+from PIL import Image, ImageTk
 
 class galacta:
   def __init__(self):
     inst_init_menu = init_menu()
     inst_game = game()
 
+#Clase para el movimiento del fondo de pantalla del menu, imagen tipo gif
+class AnimatedGIF(tk.Label):
+    def __init__(self, master, path):
+        super().__init__(master)
+        self._master = master
+        self._gif_path = path
+        self._gif_frames = []
+        self._index = 0
+        self._delay = 100
+        self._load_frames()
+        self._animate()
+
+    def _load_frames(self):
+        gif = Image.open(self._gif_path)
+        try:
+            while True:
+                self._gif_frames.append(ImageTk.PhotoImage(gif.copy()))
+                gif.seek(len(self._gif_frames)) # Move to the next frame
+        except EOFError:
+            pass
+
+    def _animate(self):
+        self.config(image=self._gif_frames[self._index])
+        self._index = (self._index + 1) % len(self._gif_frames)
+        self._master.after(self._delay, self._animate)
 
 class init_menu:
   def __init__(self):
     self.window = tk.Tk()
     self.window.title("GalactaTEC")
     self.window.configure(bg="#120043")
-    self.etiqueta = tk.Label(self.window, text="¡GalactaTEC!", font=("Courier", 16, "italic"))
-    self.etiqueta.pack(padx=20, pady=30)
-    self.btnSinglePlayer = tk.Button(self.window, text="Single Player", font=("Courier", 12, "italic"))
-    self.btnSinglePlayer.pack(pady=10)
-    self.btnMultiPlayer = tk.Button(self.window, text="Multiplayer", font=("Courier", 12, "italic"))
-    self.btnMultiPlayer.pack(pady=10)
-    self.btnNewProfile = tk.Button(self.window, text="Profiles", font=("Courier", 12, "italic"), command= self.clear_win)
-    self.btnNewProfile.pack(pady=10)
+    self.window.geometry("800x600")
+    
+    #Configuracion del fondo 
+    gif_path = "Images/space_background.gif"
+    animated_gif = AnimatedGIF(self.window, gif_path)
+    animated_gif.place(x=0, y=0, relwidth=1, relheight=1) 
+    
+    #Etiquetas y botones del menu principal
+    self.etiqueta = tk.Label(self.window, text="¡GalactaTEC!", font=("Fixedsys", 30, "italic"), bg="#120043", fg="white")
+    self.etiqueta.place(relx=0.5, rely=0.2, anchor="center")  # Centra la etiqueta horizontalmente y la coloca 30% desde la parte superior
+    
+    self.btnUserSettings = tk.Button(self.window, text="User Settings", font=("Fixedsys", 15))
+    self.btnUserSettings.place(relx=0.5, rely=0.3, anchor="center", width=230)  # Centra el botón horizontalmente y lo coloca 40% desde la parte superior
+  
+    self.btnGameSettings = tk.Button(self.window, text="Game Settings", font=("Fixedsys", 15))
+    self.btnGameSettings.place(relx=0.5, rely=0.4, anchor="center", width=230)  # Centra el botón horizontalmente y lo coloca 50% desde la parte superior
+
+    self.btnHallofFame = tk.Button(self.window, text="Hall of Fame", font=("Fixedsys", 15), command=self.clear_win)
+    self.btnHallofFame.place(relx=0.5, rely=0.5, anchor="center", width=230)  # Centra el botón horizontalmente y lo coloca 60% desde la parte superior
+
+    self.btnInitP2 = tk.Button(self.window, text="Start Player 2", font=("Fixedsys", 15), command=self.clear_win)
+    self.btnInitP2.place(relx=0.5, rely=0.6, anchor="center", width=230)  # Centra el botón horizontalmente y lo coloca 60% desde la parte superior
+
+    self.btnStartGame = tk.Button(self.window, text="Start Game", font=("Fixedsys", 15), command=self.clear_win)
+    self.btnStartGame.place(relx=0.5, rely=0.7, anchor="center", width=230)  # Centra el botón horizontalmente y lo coloca 60% desde la parte superior
+
+    self.btnExit = tk.Button(self.window, text="Exit", font=("Fixedsys", 15), command=self.clear_win)
+    self.btnExit.place(relx=0.5, rely=0.8, anchor="center", width=230)  # Centra el botón horizontalmente y lo coloca 60% desde la parte superior
+
     self.window.mainloop()
   
   def clear_win(self):
