30e49bcca6a88d32b0e5df9f47e702d52c88c3f9
valeriam07 <47421582+valeriam07@users.noreply.github.com>
2024-04-26 12:46:30 -0600

    Menú para multijugador

--- menu.py
+++ menu.py
@@ -3,7 +3,8 @@ import AnimatedGIF
 
 class init_menu:
   def __init__(self, window, animated_gif, key):
-    self.key = key #llave para el jugador 1 (jugador principal)
+    self.key = key 
+    self.key2 = 0
     self.window = window
     self.animated_gif = animated_gif
 
@@ -13,8 +14,9 @@ class init_menu:
     
     #Etiquetas y botones del menu principal
     self.etiqueta = tk.Label(self.window, text="¡GalactaTEC!", font=("Fixedsys", 30, "italic"), bg="#120043", fg="white")
-    self.etiqueta.place(relx=0.5, rely=0.2, anchor="center")  # Centra la etiqueta horizontalmente y la coloca 30% desde la parte superior
-    
+    self.etiqueta.place(relx=0.5, rely=0.1, anchor="center") 
+
+  def showMenu(self):
     self.btnUserSettings = tk.Button(self.window, text="User Settings", font=("Fixedsys", 15), command=self.userSettings)
     self.btnUserSettings.place(relx=0.5, rely=0.3, anchor="center", width=230)  # Centra el botón horizontalmente y lo coloca 40% desde la parte superior
   
@@ -43,14 +45,44 @@ class init_menu:
      login2.player = "player2"
      login2.showLogin()
 
-     
+  def multiplayer(self, key1, key2):
+    self.key2 = key2
+    self.key1 = key1
+    self.selected_player = tk.StringVar()
+    self.player1_radio = tk.Radiobutton(self.window, text="Player 1", variable=self.selected_player, value="player1", font=("Fixedsys", 12), bg="#120043", fg="white")
+    self.player1_radio.place(relx=0.4, rely=0.2, anchor="center")
 
-  
+    self.player2_radio = tk.Radiobutton(self.window, text="Player 2", variable=self.selected_player, value="player2", font=("Fixedsys", 12), bg="#120043", fg="white")
+    self.player2_radio.place(relx=0.6, rely=0.2, anchor="center")
+
+    def on_select():
+        if self.selected_player.get() == "player1":
+            print("selected menu for player 1")
+            self.key = key1
+        elif self.selected_player.get() == "player2":
+            print("selected menu for player 2")
+            self.key = key2
+
+    # Configura una función que se llamará cuando cambie la selección del jugador
+    self.selected_player.trace("w", lambda *args: on_select())
 
   def userSettings(self):
      import newUser
      self.clear_win()
-     newUser.newUser(self.window, self.animated_gif, self.key)
+     config = newUser.newUser(self.window, self.animated_gif, self.key)
+     config.key1 = self.key1
+     config.key2 = self.key2
+     config.showConfig()
+     
+  
+  def hallofFame(self):
+     import hallofFame
+     self.clear_win()
+     hall = hallofFame.hallofFame(self.window, self.animated_gif, self.key)
+     hall.key1 = self.key1
+     hall.key2 = self.key2
+     hall.showHall()
+
   
   def clear_win(self):
       for widget in self.window.winfo_children():
@@ -59,3 +91,4 @@ class init_menu:
     
   def gotoProfiles(self):
     self.clear_win()
+     
