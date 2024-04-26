016e6c4d91200a20c5668cdde8b24de5b0e95076
valeriam07 <47421582+valeriam07@users.noreply.github.com>
2024-04-26 13:04:22 -0600

    Botón para regresar de Hall of Fame al menú

--- menu.py
+++ menu.py
@@ -3,7 +3,9 @@ import AnimatedGIF
 
 class init_menu:
   def __init__(self, window, animated_gif, key):
-    self.key = key #llave para el jugador 1 (jugador principal)
+    self.key = key 
+    self.key1 = key
+    self.key2 = 0
     self.window = window
     self.animated_gif = animated_gif
 
@@ -16,22 +18,27 @@ class init_menu:
     self.etiqueta.place(relx=0.5, rely=0.2, anchor="center")  # Centra la etiqueta horizontalmente y la coloca 30% desde la parte superior
     
     self.btnUserSettings = tk.Button(self.window, text="User Settings", font=("Fixedsys", 15), command=self.userSettings)
-    self.btnUserSettings.place(relx=0.5, rely=0.3, anchor="center", width=230)  # Centra el botón horizontalmente y lo coloca 40% desde la parte superior
+    self.btnUserSettings.place(relx=0.5, rely=0.3, anchor="center", width=230)  
   
     self.btnGameSettings = tk.Button(self.window, text="Game Settings", font=("Fixedsys", 15), command=self.clear_win)
-    self.btnGameSettings.place(relx=0.5, rely=0.4, anchor="center", width=230)  # Centra el botón horizontalmente y lo coloca 50% desde la parte superior
+    self.btnGameSettings.place(relx=0.5, rely=0.4, anchor="center", width=230) 
 
     self.btnHallofFame = tk.Button(self.window, text="Hall of Fame", font=("Fixedsys", 15), command=self.hallofFame)
-    self.btnHallofFame.place(relx=0.5, rely=0.5, anchor="center", width=230)  # Centra el botón horizontalmente y lo coloca 60% desde la parte superior
+    self.btnHallofFame.place(relx=0.5, rely=0.5, anchor="center", width=230)  
 
-    self.btnInitP2 = tk.Button(self.window, text="Start Player 2", font=("Fixedsys", 15), command=self.initPlayer2)
-    self.btnInitP2.place(relx=0.5, rely=0.6, anchor="center", width=230)  # Centra el botón horizontalmente y lo coloca 60% desde la parte superior
+    self.btnStartGame = tk.Button(self.window, text="Start Game", font=("Fixedsys", 15), command=self.clear_win) 
 
-    self.btnStartGame = tk.Button(self.window, text="Start Game", font=("Fixedsys", 15), command=self.clear_win)
-    self.btnStartGame.place(relx=0.5, rely=0.7, anchor="center", width=230)  # Centra el botón horizontalmente y lo coloca 60% desde la parte superior
+    self.btnExit = tk.Button(self.window, text="Exit", font=("Fixedsys", 15), command=self.clear_win) 
 
-    self.btnExit = tk.Button(self.window, text="Exit", font=("Fixedsys", 15), command=self.clear_win)
-    self.btnExit.place(relx=0.5, rely=0.8, anchor="center", width=230)  # Centra el botón horizontalmente y lo coloca 60% desde la parte superior
+    if self.key2 == 0:
+      self.btnInitP2 = tk.Button(self.window, text="Start Player 2", font=("Fixedsys", 15), command=self.initPlayer2)
+      self.btnInitP2.place(relx=0.5, rely=0.6, anchor="center", width=230)  
+      self.btnStartGame.place(relx=0.5, rely=0.7, anchor="center", width=230) 
+      self.btnExit.place(relx=0.5, rely=0.8, anchor="center", width=230) 
+
+    else:
+      self.btnStartGame.place(relx=0.5, rely=0.6, anchor="center", width=230)
+      self.btnExit.place(relx=0.5, rely=0.7, anchor="center", width=230) 
 
     self.window.mainloop()
 
